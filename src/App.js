3616d2f120ae1d2e456db76e22fbdcb0f528d07c
Yehor Tuz <tuzegor@gmail.com>
2021-12-04 13:45:29 +0200

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countGoodFeedback = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  countNeutralFeedback = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  countBadFeedback = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.state.good > 0) {
+      return Math.round((this.state.good * 100) / this.countTotalFeedback());
+    }
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className="App">
+        <div className="feedback">
+          <h2 className="title">Please leave feedback</h2>
+          <button
+            className="goodBtn"
+            type="button"
+            onClick={this.countGoodFeedback}
+          >
+            Good
+          </button>
+          <button
+            className="neutralBtn"
+            type="button"
+            onClick={this.countNeutralFeedback}
+          >
+            Neutral
+          </button>
+          <button
+            className="badBtn"
+            type="button"
+            onClick={this.countBadFeedback}
+          >
+            Bad
+          </button>
+        </div>
+        <div className="statistics">
+          <h2>Statistics</h2>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {this.countTotalFeedback()}</p>
+          <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
